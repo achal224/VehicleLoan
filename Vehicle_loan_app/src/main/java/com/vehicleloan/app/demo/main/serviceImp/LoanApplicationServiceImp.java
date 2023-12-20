aef2ee2a20709ba7ad412c2c63e2ac346a2bae26
ss <ss@ASUS>
2023-12-20 12:48:36 +0530

    mapper

--- Vehicle_loan_app/src/main/java/com/vehicleloan/app/demo/main/serviceImp/LoanApplicationServiceImp.java
+++ Vehicle_loan_app/src/main/java/com/vehicleloan/app/demo/main/serviceImp/LoanApplicationServiceImp.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vehicleloan.app.demo.main.HomeRepository.CustomerRepository;
 import com.vehicleloan.app.demo.main.model.Customer;
@@ -22,6 +23,18 @@ public class LoanApplicationServiceImp implements LoanApplicationServiceInterfac
 			MultipartFile thumb, MultipartFile bankCheque, MultipartFile salarySlips) {
 		
 		ObjectMapper mapper=new ObjectMapper();
+		
+		try {
+			Customer c =mapper.readValue(userJson, Customer.class);
+			
+			
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
  
 		 
 		return null;
