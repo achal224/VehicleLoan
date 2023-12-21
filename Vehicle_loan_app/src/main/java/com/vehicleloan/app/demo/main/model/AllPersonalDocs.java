f10eccc316899ce9148353f085aa379c8f9f01db
ss <ss@ASUS>
2023-12-21 20:10:20 +0530

    long blob length

--- Vehicle_loan_app/src/main/java/com/vehicleloan/app/demo/main/model/AllPersonalDocs.java
+++ Vehicle_loan_app/src/main/java/com/vehicleloan/app/demo/main/model/AllPersonalDocs.java
@@ -1,5 +1,6 @@
 package com.vehicleloan.app.demo.main.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,22 +20,31 @@ public class AllPersonalDocs {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int documentId;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] addressProff;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] panCard;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] IncomeTax;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] adharcard;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] photo;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] signature;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] thumb;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] bankCheque;
 	@Lob
+	@Column(length = 999999999)
 	private byte[] salarySlips;
 }
     
